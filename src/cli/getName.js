3cceb985420388ed6d4713da1a0396daaf2d83ac
Andrei Yasinski <andrew.yasinski@gmail.com>
2024-02-04 15:49:30 +0100

    update index.js

--- src/cli/getName.js
+++
@@ -1,7 +0,0 @@
-export const getName = () => {
-  const name = process.argv
-    .find((item) => item.startsWith("--username="))
-    .split("=")?.[1];
-
-  return name || "unknown";
-};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import { getName } from "./cli/getName.js";
-
 const fileManager = () => {
-  const userName = getName();
-  console.log(`Welcome to the File Manager, ${userName}!`);
+  console.log("Hello");
 };
 
 fileManager();
