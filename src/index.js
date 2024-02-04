ef0e224a519bfe52a99b3d23969eb22db8e90226
Andrei Yasinski <andrew.yasinski@gmail.com>
2024-02-04 16:08:05 +0100

    add .exit command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const fileManager = () => {
-  console.log("Hello");
+import { getName } from "./utils/getName.js";
+import { stdin, stdout, exit } from "process";
+
+const fileManager = async () => {
+  const userName = getName();
+  console.log(`Welcome to the File Manager, ${userName}!`);
+
+  process.stdin.on("data", (chunk) => {
+    const command = chunk.toString();
+
+    if (command.includes(".exit")) {
+      console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+      exit();
+    }
+  });
 };
 
 fileManager();
